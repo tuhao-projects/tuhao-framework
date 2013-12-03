b004cdbb19c38ee2e5bd01749ec947b4c45b78d9
tuhao-projects <shijiachen001@gmail.com>
2013-12-03 18:22:02 +0800

    add some code

--- tuhao-core/src/test/java/org/tuhao/framework/core/interfaces/SqlStandardKeywordTest.java
+++ tuhao-core/src/test/java/org/tuhao/framework/core/interfaces/SqlStandardKeywordTest.java
@@ -1,5 +1,12 @@
 package org.tuhao.framework.core.interfaces;
 
+import org.junit.Test;
+
 public class SqlStandardKeywordTest{
 
+	@Test
+	public void showAllKeywordTest(){
+		System.out.println(SqlStandardKeyword.Operation.SELECT);
+	}
+	
 }
